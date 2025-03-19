08350bef280264283ee493ac7203d29315b8b944
Sanjay K <2001ksanjay@gmail.com>
2025-03-19 10:41:33 +0530

    Update app-constants.ts

--- src/app-constants.ts
+++ src/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants{
 
-    static BACKEND_API_URL= 'http://3.110.33.27:8081/products';
+    static BACKEND_API_URL= 'http://database-1.c3kaay88gbjs.ap-south-1.rds.amazonaws.com:3306/vinodses_ecomm';
 }
